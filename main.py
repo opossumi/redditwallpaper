3c464c4cee2dc2956de95fd1711007c5ca4e2ae8
Erkka Saarela <koodi@erkka.fi>
2020-11-13 00:26:31 +0200

    Add ratelimit and upgrade to https

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 """"
 Downloads top wallpapers from r/wallpapers and saves them to ~/.wallpapers
 
-Intended usage (in crontab):
-HOME=/home/user
+Intended usage (run as user, not root):
 0 * * * * /usr/local/bin/redditwallpaper_delayed
 */5 * * * * /usr/local/bin/change_wallpaper
 """
@@ -14,11 +13,43 @@ import re
 import requests
 from urllib.parse import urlparse
 
-headers = {'user-agent': 'redditwallpaper/0.1.0'}
+# Copy-pasted from stackoverflow ":D"
+import time
+from threading import Lock
+mutex = Lock()
+nextTime = [0.0]
+def RateLimited(limit=None):
+    def decorate(func):
+        global nextTime
+        def rateLimitedFunction(*args,**kargs):
+            mutex.acquire()
+            maxPerSecond = limit
+            if not maxPerSecond:
+                if hasattr(args[0], 'ratelimit'):
+                    maxPerSecond = getattr(args[0], 'ratelimit')
+                else:
+                    maxPerSecond = 1 # Default value
+            minInterval = 1.0 / float(maxPerSecond)
+            leftToWait = nextTime[0] - time.perf_counter()
+            nextTime[0] = max(nextTime[0], time.perf_counter())+minInterval
+            mutex.release()
+            if leftToWait>0:
+                time.sleep(leftToWait)
+            ret = func(*args,**kargs)
+            return ret
+        return rateLimitedFunction
+    return decorate
+
+s = requests.Session()
+s.headers = {'user-agent': 'redditwallpaper/0.2.0'}
 wallpaper_folder = os.path.join(os.path.expanduser("~"), ".wallpapers")
 
+@RateLimited(0.5)
 def fetch(url):
-    r = requests.get(url, headers=headers)
+    r = s.get(url)
+    if r.status_code != 200:
+        print ('Not working, abort mission')
+        os.exit(1)
     if not r.headers['content-type'] in ('image/jpeg', 'image/png', 'image/bmp'):
         return False
 
@@ -36,16 +67,17 @@ def fetch(url):
     return True
 
 def fetch_images(url):
-    r = requests.get(url, headers=headers)
+    r = s.get(url)
     data = r.json()
     for c in data['data']['children']:
-        for img in c['data']['preview']['images']:
-            url = img['source']['url']
+        url = (c['data']['url'])
+        if re.match('.*\.(jpg|png)', url):
             fname = os.path.basename(urlparse(url).path)
+            #print (url, fname)
             path = os.path.join(wallpaper_folder, fname)
             if not os.path.isfile(path):
                 print ("Fetching", url)
-                fetch(img['source']['url'])
+                fetch(url)
             else:
                 print ("Skipping", fname)
 
@@ -56,7 +88,8 @@ def main():
 
     args = parser.parse_args()
 
-    reddit_json_url = 'http://www.reddit.com/r/' + args.subreddit + '/top.json?sort=top&t=' + args.freq
+    reddit_json_url = 'https://www.reddit.com/r/' + args.subreddit + '/top.json?sort=top&t=' + args.freq
+    print (reddit_json_url)
     fetch_images(reddit_json_url)
 
 if __name__ == '__main__':
